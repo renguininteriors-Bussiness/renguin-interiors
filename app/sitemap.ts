a6e6e7c12f7a0e6a042c12ac73a5c4cb6572a7ea
Narayanaguru <renguininteriors@.com>
2026-07-05 19:41:54 +0530

    Added sitemap and robots

---
+++ app/sitemap.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://renguin-interiors.vercel.app";
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
